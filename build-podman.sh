391ff21d139f82d64c5cc9baebdb356a8a453e45
Ildar Akhmetgaleev <akhilman@gmail.com>
2025-08-12 10:45:42 +0700

    Build Helix in Debian Trixie

--- build-podman.sh
+++ build-podman.sh
@@ -5,5 +5,5 @@ podman run \
   --rm \
   -v $(dirname $0)/:/pkg/:rw \
   --name helix-makedeb \
-  ghcr.io/makedeb/makedeb:debian-bullseye \
+  ghcr.io/makedeb/makedeb:debian-trixie \
   bash -c "sudo apt update && sudo apt dist-upgrade -y && cp /pkg/PKGBUILD ./ && makedeb -s --no-confirm && sudo cp *.deb /pkg/"

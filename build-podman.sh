2d40fad03d47448cf32eb3443608d69137ed7300
Ildar Akhmetgaleev <akhilman@gmail.com>
2025-08-19 11:38:19 +0700

    Allow custom args and environment variables in build scrip

--- build-podman.sh
+++ build-podman.sh
@@ -5,5 +5,6 @@ podman run \
   --rm \
   -v $(dirname $0)/:/pkg/:rw \
   --name helix-makedeb \
+  $@ \
   ghcr.io/makedeb/makedeb:debian-trixie \
-  bash -c "sudo apt update && sudo apt dist-upgrade -y && cp /pkg/PKGBUILD ./ && makedeb -s --no-confirm && sudo cp *.deb /pkg/"
+  bash -c "sudo -E apt update && sudo -E apt dist-upgrade -y && cp /pkg/PKGBUILD ./ && makedeb -s --no-confirm && sudo cp *.deb /pkg/"
